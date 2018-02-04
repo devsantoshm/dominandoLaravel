dfce498809447214c4900496b97ca9b1accbb0ef
santoshm <dosantoshm@gmail.com>
2018-02-03 19:00:00 -0500

    como se utilizan las rutas

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,16 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    echo "<a href=". route('contactos') . ">Contacto</a><br>";
+    echo "<a href=". route('contactos') . ">Contacto</a><br>";
+    echo "<a href=". route('contactos') . ">Contacto</a><br>";
 });
+//Darle nombre a la ruta para evitar cambiar en todas las páginas
+Route::get('contactanos', ['as' => 'contactos', function () {
+    return "Hola desde contactanos";
+}]);
+//Agregamos un ? para que el parametro nombre no sea obligatorio definirlo por la url
+//Agregamos where para validar el nombre, para que sea solamente letras y no numeros
+Route::get('saludos/{nombre?}', function ($nombre = "Invitado") {
+    return "saludos $nombre";
+})->where('nombre', "[A-Za-z]+");
