a07bc4d55bc3f8a5e33d48a3a9a19d028e19fb58
santoshm <dosantoshm@gmail.com>
2018-02-16 19:28:49 -0500

    Aplicando make auth login automático

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -17,19 +17,6 @@ class PagesController extends Controller
     	return view('home');
     }
 
-    public function contact()
-    {
-    	return view('contactos');
-    }
-
-    public function mensajes(CreateMessageRequest $request)
-    {
-        $data = $request->all(); //devuelve un array
-        /*return redirect()->route('contactos')
-                        ->with('info', 'Tu mensaje se envio correctamente');*/
-        return back()->with('info', 'Tu mensaje se envio correctamente');
-    }
-
     public function saludo($nombre = "Invitado")
     {
     	return view('saludo', compact('nombre'));
--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,6 @@
 
 Route::get('/', ['as' => 'home', 'uses' => 'PagesController@home']);
 //Darle nombre a la ruta para evitar cambiar en todas las páginas
-Route::get('contactanos', ['as' => 'contactos', 'uses' => 'PagesController@contact']);
-Route::post('contacto', 'PagesController@mensajes');
 //Agregamos un ? para que el parametro nombre no sea obligatorio definirlo por la url
 //Agregamos where para validar el nombre, para que sea solamente letras y no numeros
 Route::get('saludos/{nombre?}', ['as' => 'saludos', 'uses' => 'PagesController@saludo'])->where('nombre', "[A-Za-z]+");
@@ -28,3 +26,19 @@ Route::get('mensajes/{id}', ['as' => 'messages.show', 'uses' => 'MessagesControl
 Route::get('mensajes/{id}/edit', ['as' => 'messages.edit', 'uses' => 'MessagesController@edit']);
 Route::put('mensajes/{id}', ['as' => 'messages.update', 'uses' => 'MessagesController@update']);
 Route::delete('mensajes/{id}', ['as' => 'messages.destroy', 'uses' => 'MessagesController@destroy']);*/
+Auth::routes();
+/*$this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
+$this->post('login', 'Auth\LoginController@login');
+$this->post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registration Routes...
+$this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+$this->post('register', 'Auth\RegisterController@register');
+
+// Password Reset Routes...
+$this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+$this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+$this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+$this->post('password/reset', 'Auth\ResetPasswordController@reset');*/
+
+Route::get('/home', 'HomeController@index')->name('home');
